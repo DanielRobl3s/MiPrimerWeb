193d12ad09dbc2451e49afba38e6855ff03e6d93
danielrobl3s <danielrobles200217@gmail.com>
2022-12-09 21:48:07 -0600

    Agregando archivo HTML, CSS y JS junto con un texto random

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("Esta madre funciona bien si hiciste todo bien");
+
+function jelloWorld(tu_nombre)
+{
+    this.tu_nombre=prompt("what's your name my guy?");
+    console.log("Se tu nombre, te llamas: "+this.tu_nombre);
+}
+
+jelloWorld();
+
